8f12087a6f295887d2f70dea7ccd02f607ec7009
wyt309 <309301362@qq.com>
2018-09-20 16:26:12 +0800

    no message

--- wutongchain/src/main/java/com/tjfintech/common/SoloSign.java
+++ wutongchain/src/main/java/com/tjfintech/common/SoloSign.java
@@ -4,17 +4,11 @@ import com.tjfintech.common.utils.PostTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import com.tjfintech.common.utils.GetTest;
 import java.util.*;
 
 @Slf4j
 public class SoloSign {
-    private static String   TOKENTYPE;
-    private static final String USER1_1="32XBduKk48RJfoA2wJuQ7doaJimMymEmZLYYbYHyH5Eyi5Hs68";
-    private static final String USER1_2="SogjzbsX6RsWwRKqdhTQeVTWQhy2SyNGfPy9LHEz6p3famV3wCe";
-    private static final String USER3_3="SsUTN9RmWgrD8E48MuJY1pdLw4QDo7GJgK8fn8k7DFzpvG3pwqw";
-    private static final String USER_COLLET="Soirv9ikykVHArKCdJqVNegxxqZWUj1g4ixFFYbBLMExy4zUTUe";
-    private static final String PUBKEY="LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZrd0V3WUhLb1pJemowQ0FRWUlLb0VjejFVQmdpMERRZ0FFaTA4d0pVQ1RHQWd4Yzg2ZzRQNVBUUncwSTZQMAo4MXdCZzJYWVc0Zkx1TElvZDJxR0NIR0N2dTFBWi9nQUVTRS9OT1Q2eDdMRFhVVjRPTE9Kdk5oUXBRPT0KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg==";
 
     @Before
     /**
@@ -48,51 +42,49 @@ public class SoloSign {
     /**查询单签地址余额
      * @param tokenType 币种
      */
-//    public void TestSoloQuery(String tokenType){
-//        String param;
-//        String priKey= MultiSign.PRIKEY0;
-//        Map<String,Object>map=new HashMap<>();
-//        map.put("key",priKey);
-//        map.put("tokentype",tokenType);
-//        param= GetTest.ParamtoUrl(map);
-//        log.info(GetTest.SendGetTojson(MultiSign.SDKADD+"/utxo/balance"+"?"+param));
-//    }
+    public void balance(String key,String tokenType){
+        String param;
+        Map<String,Object>map=new HashMap<>();
+        map.put("key",key);
+        map.put("tokentype",tokenType);
+        param= GetTest.ParamtoUrl(map);
+        log.info(GetTest.SendGetTojson(MultiSign.SDKADD+"/utxo/balance"+"?"+param));
+    }
     /**单签账号向其他地址转账
      *
      *
      */
-//    public void TestSoloTransfer(String TokenType,String ToAddr) {
-//        String PriKey = MultiSign.PRIKEY0;
-//        String Data = "使用单签账户私钥发起对多签账户转账-无密码";
-//        String Amount = "10.0";
-//        Map<String, Object> token = new HashMap<>();
-//        token.put("ToAddr", ToAddr);
-//        token.put("TokenType", TokenType);
-//        token.put("Amount", Amount);
-//        List<Map> TokenObjects = new ArrayList<Map>();
-//        TokenObjects.add(token);
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("Prikey", PriKey);
-//        map.put("Data", Data);
-//        map.put("Token", TokenObjects);
-//
-//        log.info(PostTest.sendPostToJson(MultiSign.SDKADD+"/utxo/transfer", map));
-//
-//    }
-//
+    public void Transfer(String token,String priKey,String data) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("Prikey", priKey);
+        map.put("Data", data);
+        map.put("Token", token);
+
+        log.info(PostTest.sendPostToJson(MultiSign.SDKADD+"/utxo/transfer", map));
+
+    }
+
+    /**
+     * 发行token
+     */
+    public void issueToken(String priKey,String tokenType,String amount,String data){
+        Map<String, Object> map = new HashMap<>();
+        map.put("PriKey", priKey);
+        map.put("TokenType", tokenType);
+        map.put("Amount", amount);
+        log.info(PostTest.sendPostToJson(MultiSign.SDKADD+"/utxo/genaddress", map));
+    }
 
 
 
 
-    @Test
     /**
      * 创建1/1单签账号
      */
-    public void Create1_1User(){
+    public void genAddress(String publicKey){
         Map<String, Object> map = new HashMap<>();
-        map.put("Pubkey", PUBKEY);
+        map.put("PubKey", publicKey);
         log.info(PostTest.sendPostToJson(MultiSign.SDKADD+"/utxo/genaddress", map));
-
     }
 
     @After
