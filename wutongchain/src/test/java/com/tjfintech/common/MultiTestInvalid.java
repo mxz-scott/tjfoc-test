375bfca6b119488ed03e2e1705fdc440c7967058
chenxu <czdyxs@qq.com>
2018-09-20 17:29:13 +0800

    新增异常测试

--- wutongchain/src/test/java/com/tjfintech/common/MultiTestInvalid.java
+++ wutongchain/src/test/java/com/tjfintech/common/MultiTestInvalid.java
@@ -1,4 +1,21 @@
 package com.tjfintech.common;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@Slf4j
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MultiTestInvalid {
+    @Test
+    public void TC37_transferMultiInvalid(){
+
+    }
+    @Test
+    public  void TC38_transferSoloMultiInvalid(){
+
+    }
+    @Test
+    public void TC39_transferSoloInvalid(){}
 }
