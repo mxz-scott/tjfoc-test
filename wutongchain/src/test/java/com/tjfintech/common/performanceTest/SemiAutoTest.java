430b112261b248062b1490245b413892e318cf0f
chenxu <czdyxs@qq.com>
2018-10-08 16:31:00 +0800

    新增半自动化测试Demo

---
+++ wutongchain/src/test/java/com/tjfintech/common/performanceTest/SemiAutoTest.java
@@ -0,0 +1,18 @@
+package com.tjfintech.common.performanceTest;
+
+import block.BlockOuterClass;
+import com.google.gson.Gson;
+import com.tjfoc.sdk.Client;
+import lombok.extern.slf4j.Slf4j;
+import java.util.Scanner;
+
+@Slf4j
+public class SemiAutoTest {
+
+    public static void main(String[] args) throws  Exception{
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+        
+    }
+
+}
