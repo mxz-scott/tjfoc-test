043f4607423a6fda59053657952a141e18401519
chenxu <czdyxs@qq.com>
2019-04-09 13:26:37 +0800

    新增GET请求代码，实现异常状态码请求返回

--- wutongchain/src/main/java/com/tjfintech/common/utils/GetTest.java
+++ wutongchain/src/main/java/com/tjfintech/common/utils/GetTest.java
@@ -1,76 +1,142 @@
 package com.tjfintech.common.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.http.HttpStatus;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
+
 @Slf4j
 public class GetTest {
     public static String SendGetTojson(String url) {
-            BufferedReader in = null;
-            try {
-                URL realUrl = new URL(url);
-                // 打开和URL之间的连接
-                URLConnection connection = realUrl.openConnection();
-                // 设置通用的请求属性
-                HttpURLConnection httpURLConnection = (HttpURLConnection)connection;
-                httpURLConnection.setRequestMethod("GET");
-                httpURLConnection.setRequestProperty("Accept-Charset", "UTF-8");
-                connection.setRequestProperty("Charset", "UTF-8");
-                // 设置文件类型:
-                connection.setRequestProperty("Content-Type","application/json; charset=UTF-8");
-                connection.setRequestProperty("accept", "*/*");
-                connection.setRequestProperty("connection", "Keep-Alive");
-                connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-                connection.setConnectTimeout(5000);
-                connection.setReadTimeout(5000);
-                // 建立实际的连接
-                connection.connect();
-                // 定义 BufferedReader输入流来读取URL的响应
-                in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuffer sb = new StringBuffer();
-                String line;
-                while ((line = in.readLine()) != null) {
-                    sb.append(line);
-                }
-                return sb.toString();
-            } catch (Exception e) {
-                log.error("发生错误",e);
+        BufferedReader in = null;
+        try {
+            URL realUrl = new URL(url);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            HttpURLConnection httpURLConnection = (HttpURLConnection) connection;
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setRequestProperty("Accept-Charset", "UTF-8");
+            connection.setRequestProperty("Charset", "UTF-8");
+            // 设置文件类型:
+            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            // 建立实际的连接
+            connection.connect();
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            log.error("发生错误", e);
 
-            }
-            // 使用finally块来关闭输入流
-            finally {
-                try {
-                    if (in != null) {
-                        in.close();
-                    }
-                } catch (Exception e2) {
-                    log.error("发生错误",e2);
-                }
-            }
-            return null;
         }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                log.error("发生错误", e2);
+            }
+        }
+        return null;
+    }
 
-        public static   String ParamtoUrl(Map<String,Object> map){
-        String param="";
-        if(map.equals(null)){
+    public static String ParamtoUrl(Map<String, Object> map) {
+        String param = "";
+        if (map.equals(null)) {
             return null;
-        }else {
-            for(Map.Entry<String, Object> entry:map.entrySet()){
+        } else {
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
                 String key = entry.getKey();
                 Object value = entry.getValue();
-                if(param.equals("")){
-                    param=key+"="+value.toString();
-                }else {
-                    param=param+"&"+key+"="+value.toString();
+                if (param.equals("")) {
+                    param = key + "=" + value.toString();
+                } else {
+                    param = param + "&" + key + "=" + value.toString();
                 }
-            }}
-            return param;
+            }
         }
-
-
+        return param;
     }
+
+    public static String doGet2(String url) {
+        /*
+         * 使用 GetMethod 来访问一个 URL 对应的网页,实现步骤: 1:生成一个 HttpClinet 对象并设置相应的参数。 2:生成一个
+         * GetMethod 对象并设置响应的参数。 3:用 HttpClinet 生成的对象来执行 GetMethod 生成的Get 方法。 4:处理响应状态码。
+         * 5:若响应正常，处理 HTTP 响应内容。 6:释放连接。
+         */
+        /* 1 生成 HttpClinet 对象并设置参数 */
+        BufferedReader in = null;
+        HttpClient httpClient = new HttpClient();
+        // 设置 Http 连接超时为5秒
+        httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(5000);
+
+        /* 2 生成 GetMethod 对象并设置参数 */
+        GetMethod getMethod = new GetMethod(url);
+        // 设置 get 请求超时为 5 秒
+        getMethod.getParams().setParameter(HttpMethodParams.SO_TIMEOUT, 5000);
+        // 设置请求重试处理，用的是默认的重试处理：请求三次
+        getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
+        String response = "";
+        /* 3 执行 HTTP GET 请求 */
+        try {
+            int statusCode = httpClient.executeMethod(getMethod);
+            /* 4 判断访问的状态码 */
+            if (statusCode != HttpStatus.SC_OK) {
+                System.err.println("Method failed: " + getMethod.getStatusLine());
+            }
+            /* 5 处理 HTTP 响应内容 */
+            // HTTP响应头部信息，这里简单打印
+            Header[] headers = getMethod.getResponseHeaders();
+            // for (Header h : headers)
+            // System.out.println(h.getName() + "------------ " + h.getValue());
+            // 读取 HTTP 响应内容，这里简单打印网页内容\
+            InputStream responseStream = getMethod.getResponseBodyAsStream();
+            in = new BufferedReader(new InputStreamReader(responseStream));
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+            }
+            response = sb.toString();
+            log.info(response);
+
+        } catch (HttpException e) {
+            // 发生致命的异常，可能是协议不对或者返回的内容有问题
+            System.out.println("Please check your provided http address!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            // 发生网络异常
+            e.printStackTrace();
+        } finally {
+            /* 6 .释放连接 */
+            getMethod.releaseConnection();
+        }
+        return response;
+    }
+
+}
