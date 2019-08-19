5a34c172d26cf5736b2bcecda0cbcbb1875251db
samzhou1221 <zhouxianxian@wutongchain.com>
2019-08-19 17:43:25 +0800

    修改mongo数据库默认文件。

--- wutongchain/src/test/java/com/tjfintech/common/functionTest/Conditions/SetDatabaseMongo.java
+++ wutongchain/src/test/java/com/tjfintech/common/functionTest/Conditions/SetDatabaseMongo.java
@@ -11,15 +11,11 @@ public class SetDatabaseMongo {
    @Test
     public void setDBMongo()throws Exception{
        //设置SDK 使用Mongo 清空数据库
-       shellExeCmd(getIPFromStr(SDKADD),resetSDKConfig);
+       shellExeCmd(getIPFromStr(SDKADD),"cp " + SDKPATH + "conf/configMongo.toml " + SDKPATH + "conf/config.toml");
        delDataBase();//清空sdk当前使用数据库数据
 
-       //设置节点 停止所有节点 清空所有节点db数据 并重启所有节点
-       peerList.clear();
-       peerList.add(PEER1IP);
-       peerList.add(PEER2IP);
-       peerList.add(PEER4IP);
-       sendCmdPeerList(peerList,clearPeerDB,resetPeerBase,startPeerCmd );
+      //设置节点 清空db数据 并重启
+      setAndRestartPeerList(clearPeerDB,resetPeerBase);
 
        //重启SDK
        setAndRestartSDK();
