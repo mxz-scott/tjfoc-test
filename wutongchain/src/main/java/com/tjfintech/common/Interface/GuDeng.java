3470fa3662acc3f81faca10390c0abea642335f5
dduzou <zoulili@wutongchain.com>
2020-08-28 10:03:44 +0800

    股登测试接口实现 part2

--- wutongchain/src/main/java/com/tjfintech/common/Interface/GuDeng.java
+++ wutongchain/src/main/java/com/tjfintech/common/Interface/GuDeng.java
@@ -11,4 +11,15 @@ public interface GuDeng {
     String GDEnterpriseResister(String contractAddress,Map basicInfo, Map businessInfo, Map legalPersonInfo, String extend);
     String GDCreateAccout(String contractAddress,Map investorInfo);
 
+    String GDShareIssue(String contractAddress,String platformKeyId,String equityCode,List<Map>shareList);
+    String GDShareChangeProperty(String platformKeyId, String address, String equityCode, double amout, int oldShareProperty, int newShareProperty);
+
+    String GDShareChangeBoard(String platformKeyId,String companyId,String oldEquityCode,String newEquityCode);
+
+    String GDShareTransfer(String keyId,String fromAddr,double amount,String toAddr, int shareProperty,String equityCode,int txType,
+                           String orderNo,int orderWay,int orderType,String price,String time,String remark);
+    String GDShareIncrease(String contractAddress,String platformKeyId,String equityCode,List<Map>shareList,String reason);
+
+    String GDAccountDestroy(String contractAddress,String clientNo);
+
 }
--- wutongchain/src/test/java/com/tjfintech/common/functionTest/guDengTest/enterpriseRegisterTest.java
+++ wutongchain/src/test/java/com/tjfintech/common/functionTest/guDengTest/enterpriseRegisterTest.java
@@ -1,6 +1,5 @@
 package com.tjfintech.common.functionTest.guDengTest;
 
-import com.alibaba.fastjson.JSON;
 import com.tjfintech.common.CommonFunc;
 import com.tjfintech.common.Interface.GuDeng;
 import com.tjfintech.common.Interface.Store;
@@ -16,7 +15,6 @@ import java.util.*;
 
 import static com.tjfintech.common.utils.UtilsClass.*;
 import static org.hamcrest.CoreMatchers.anyOf;
-import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
@@ -26,17 +24,23 @@ public class enterpriseRegisterTest {
 
     TestBuilder testBuilder= TestBuilder.getInstance();
     GuDeng gd =testBuilder.getGuDeng();
+    Store store =testBuilder.getStore();
     UtilsClass utilsClass = new UtilsClass();
     CommonFunc commonFunc = new CommonFunc();
     String contractAddr = "48cb62af2f6363a5088264ce41193a362455b27fedb1d72eb512a7bfeb339523";
-    String platformKeyID = "bt3hichpgfltc7nnqlvg";
+    String platformKeyID = "bt45k19pgfltc7nnqn50";
+    String companyId = "company100001";
+    String clientNo = "c0000001";
+    String equityCode = "SZ100001";
 
 
     @Test
     public void TC01_enterpriseRegister() throws Exception {
+        String compId = companyId;
+        String eqCode = equityCode;
 
         Map mapBaseInfo = new HashMap();
-        mapBaseInfo.put("equityCode","SZ100001");
+        mapBaseInfo.put("equityCode",eqCode);
         mapBaseInfo.put("equitySimpleName","苏州股权代码");
         mapBaseInfo.put("equityType",0);
         mapBaseInfo.put("totalShares",1000000);
@@ -45,7 +49,7 @@ public class enterpriseRegisterTest {
         mapBaseInfo.put("certificateType",0);
         mapBaseInfo.put("certificateNo","1585685245666821236");
         mapBaseInfo.put("currency","人民币");
-        mapBaseInfo.put("companyId","1598222555555");
+        mapBaseInfo.put("companyId",compId);
         mapBaseInfo.put("companyLogo","苏州同济区块链研究");
         mapBaseInfo.put("companySimpleName","苏同院");
         mapBaseInfo.put("companyENName","tjfoc");
@@ -85,18 +89,20 @@ public class enterpriseRegisterTest {
         String storeHash = jsonObject.getJSONObject("data").getString("txId");
 
         commonFunc.sdkCheckTxOrSleep(storeHash,utilsClass.sdkGetTxDetailTypeV2,SLEEPTIME);
+        assertEquals("200",JSONObject.fromObject(store.GetTxDetail(storeHash)).getString("state"));
 
     }
 
 
     @Test
-    public void TC01_createAccout() throws Exception {
+    public void TC03_createAccout() throws Exception {
 
-        String clientNo = "c0000001";
+        String cltNo = clientNo;
         String shareHolderNo = "s0000001";
+        String eqCode = equityCode;
 
         Map mapPersonInfo = new HashMap();
-        mapPersonInfo.put("clientFullName","SZ100001");
+        mapPersonInfo.put("clientFullName",eqCode);
         mapPersonInfo.put("organizationType","苏州股权代码");
         mapPersonInfo.put("certificateType",0);
         mapPersonInfo.put("certificateNo","123456468123153");
@@ -132,7 +138,7 @@ public class enterpriseRegisterTest {
         mapInvestorInfo.put("clientName","034654");
         mapInvestorInfo.put("shareholderNo",shareHolderNo);
         mapInvestorInfo.put("fundNo","f0000001");
-        mapInvestorInfo.put("clientNo",clientNo);
+        mapInvestorInfo.put("clientNo",cltNo);
         mapInvestorInfo.put("extend",extend);
         mapInvestorInfo.put("personalInfo",mapPersonInfo);
         mapInvestorInfo.put("investor",mapinvestor);
@@ -140,17 +146,155 @@ public class enterpriseRegisterTest {
 
         String response= gd.GDCreateAccout(contractAddr,mapInvestorInfo);
         JSONObject jsonObject=JSONObject.fromObject(response);
-        String storeHash = jsonObject.getJSONObject("data").getString("TxId");
-        assertEquals(clientNo,JSONObject.fromObject(response).getJSONObject("data").getJSONObject("AccountList").getString("ClientNo"));
-        assertEquals(shareHolderNo,JSONObject.fromObject(response).getJSONObject("data").getJSONObject("AccountList").getString("ShareholderNo"));
-        String keyId = JSONObject.fromObject(response).getJSONObject("data").getJSONObject("AccountList").getString("KeyId");
-        String address = JSONObject.fromObject(response).getJSONObject("data").getJSONObject("AccountList").getString("Address");
+        String storeHash = jsonObject.getJSONObject("data").getString("txId");
+        assertEquals(clientNo,JSONObject.fromObject(response).getJSONObject("data").getJSONObject("accountList").getString("clientNo"));
+        assertEquals(shareHolderNo,JSONObject.fromObject(response).getJSONObject("data").getJSONObject("accountList").getString("shareholderNo"));
+        String keyId = JSONObject.fromObject(response).getJSONObject("data").getJSONObject("accountList").getString("keyId");
+        String address = JSONObject.fromObject(response).getJSONObject("data").getJSONObject("accountList").getString("address");
 
         commonFunc.sdkCheckTxOrSleep(storeHash,utilsClass.sdkGetTxDetailTypeV2,SLEEPTIME);
+        assertEquals("200",JSONObject.fromObject(store.GetTxDetail(storeHash)).getString("state"));
+    }
+
+    @Test
+    public void TC06_shareIssue() throws Exception {
+
+        String eqCode = equityCode;
+
+        List<Map> shareList = gdConstructShareList("SnxqVBW7K7L8bRykHKttVjG81phwUYu7ZzZMB1bs1qYaA2GBbJM",5000,0);
+        List<Map> shareList2 = gdConstructShareList("So6uaUagSbBcDEt935v8sdA52cQ2QFRnVx9nBoaNmzKxomxSRkn",5000,0, shareList);
+        List<Map> shareList3 = gdConstructShareList("Sn6KRMf6heVv55V2AWzyE4mF9n8isgshAeZJVMhuW1bG2ARsd15",5000,0, shareList2);
+        List<Map> shareList4 = gdConstructShareList("SnnswixfQNaJd9v19LPEFY4UoAmxGtmEivHn6GBnYDD8aPtyjpY",5000,0, shareList3);
+
+        String response= gd.GDShareIssue(contractAddr,platformKeyID,eqCode,shareList4);
+        JSONObject jsonObject=JSONObject.fromObject(response);
+        String storeHash = jsonObject.getJSONObject("data").getString("txId");
+
+        commonFunc.sdkCheckTxOrSleep(storeHash,utilsClass.sdkGetTxDetailTypeV2,SLEEPTIME);
+        assertEquals("200",JSONObject.fromObject(store.GetTxDetail(storeHash)).getString("state"));
+    }
+
+    @Test
+    public void TC07_shareChangeProperty() throws Exception {
+
+        String eqCode = equityCode;
+        String address = "SnxqVBW7K7L8bRykHKttVjG81phwUYu7ZzZMB1bs1qYaA2GBbJM";
+        double changeAmount = 500;
+        int oldProperty = 0;
+        int newProperty = 1;
+
+        String response= gd.GDShareChangeProperty(platformKeyID,address,eqCode,changeAmount,oldProperty,newProperty);
+        JSONObject jsonObject=JSONObject.fromObject(response);
+        String storeHash = jsonObject.getJSONObject("data").getString("txId");
+
+        commonFunc.sdkCheckTxOrSleep(storeHash,utilsClass.sdkGetTxDetailTypeV2,SLEEPTIME);
+        assertEquals("200",JSONObject.fromObject(store.GetTxDetail(storeHash)).getString("state"));
+
+    }
+
+    @Test
+    public void TC08_shareTransfer()throws Exception{
+        String keyId = platformKeyID;
+        String fromAddr = "";
+        double amount = 100;
+        String toAddr = "";
+        int shareProperty = 0;
+        String eqCode = equityCode;
+        int txType = 0;
+        String orderNo = "test202008280952";
+        int orderWay = 0;
+        int orderType = 0;
+        String price = "10000";
+        String time = "20200828";
+        String remark = "转账";
+        String response= gd.GDShareTransfer(keyId,fromAddr,amount,toAddr,shareProperty,eqCode,txType,
+                orderNo,orderWay,orderType,price,time,remark);
+
+        JSONObject jsonObject=JSONObject.fromObject(response);
+        String storeHash = jsonObject.getJSONObject("data").getString("txId");
+
+        commonFunc.sdkCheckTxOrSleep(storeHash,utilsClass.sdkGetTxDetailTypeV2,SLEEPTIME);
+        assertEquals("200",JSONObject.fromObject(store.GetTxDetail(storeHash)).getString("state"));
+    }
+
+    @Test
+    public void TC09_shareIncrease() throws Exception {
+
+        String eqCode = equityCode;
+        String reason = "股份分红";
+
+        List<Map> shareList = gdConstructShareList("SnxqVBW7K7L8bRykHKttVjG81phwUYu7ZzZMB1bs1qYaA2GBbJM",1000,0);
+        List<Map> shareList2 = gdConstructShareList("So6uaUagSbBcDEt935v8sdA52cQ2QFRnVx9nBoaNmzKxomxSRkn",1000,0, shareList);
+        List<Map> shareList3 = gdConstructShareList("Sn6KRMf6heVv55V2AWzyE4mF9n8isgshAeZJVMhuW1bG2ARsd15",1000,0, shareList2);
+        List<Map> shareList4 = gdConstructShareList("SnnswixfQNaJd9v19LPEFY4UoAmxGtmEivHn6GBnYDD8aPtyjpY",1000,0, shareList3);
+
+        String response= gd.GDShareIncrease(contractAddr,platformKeyID,eqCode,shareList4,reason);
+        JSONObject jsonObject=JSONObject.fromObject(response);
+        String storeHash = jsonObject.getJSONObject("data").getString("txId");
+
+        commonFunc.sdkCheckTxOrSleep(storeHash,utilsClass.sdkGetTxDetailTypeV2,SLEEPTIME);
+        assertEquals("200",JSONObject.fromObject(store.GetTxDetail(storeHash)).getString("state"));
+    }
+
+    @Test
+    public void TC13_shareChangeBoard() throws Exception {
+
+        String oldEquityCode = equityCode;
+        String newEquityCode = "SZ100003";
+        String cpnyId = companyId;
+
+        String response= gd.GDShareChangeBoard(platformKeyID,cpnyId,oldEquityCode,newEquityCode);
+        JSONObject jsonObject=JSONObject.fromObject(response);
+        String storeHash = jsonObject.getJSONObject("data").getString("txId");
+
+        commonFunc.sdkCheckTxOrSleep(storeHash,utilsClass.sdkGetTxDetailTypeV2,SLEEPTIME);
+        assertEquals("200",JSONObject.fromObject(store.GetTxDetail(storeHash)).getString("state"));
+
+    }
+
+
+    @Test
+    public void TC205_accountDestroy() throws Exception {
+        String clntNo = clientNo;
+
+        String response= gd.GDAccountDestroy(contractAddr,clntNo);
+        JSONObject jsonObject=JSONObject.fromObject(response);
+        String storeHash = jsonObject.getJSONObject("data").getString("txId");
+
+        commonFunc.sdkCheckTxOrSleep(storeHash,utilsClass.sdkGetTxDetailTypeV2,SLEEPTIME);
+        assertEquals("200",JSONObject.fromObject(store.GetTxDetail(storeHash)).getString("state"));
 
     }
 
 
 
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public  List<Map>   gdConstructShareList(String address, double amount, int shareProperty){
+
+        Map<String,Object> shares = new HashMap<>();
+        shares.put("address",address);
+        shares.put("amount",amount);
+        shares.put("shareProperty",shareProperty);
+
+        List<Map> shareList = new ArrayList<>();
+        shareList.add(shares);
+        return shareList;
+    }
+
+    public  List<Map>   gdConstructShareList(String address, double amount, int shareProperty, List<Map> list){
+        List<Map> shareList = new ArrayList<>();
+        for(int i = 0 ; i < list.size() ; i++) {
+            shareList.add(list.get(i));
+        }
+        Map<String,Object> shares = new HashMap<>();
+        shares.put("address",address);
+        shares.put("amount",amount);
+        shares.put("shareProperty",shareProperty);
+
+        shareList.add(shares);
+        return shareList;
+    }
 
 }
