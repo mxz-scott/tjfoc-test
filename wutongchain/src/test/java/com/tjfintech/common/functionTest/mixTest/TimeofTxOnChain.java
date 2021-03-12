a2dc88bcf2033c462bbe88eafac874f678b3e4ba
dduzou <zoulili@wutongchain.com>
2021-03-12 16:54:39 +0800

    优化时间统计

--- wutongchain/src/test/java/com/tjfintech/common/functionTest/mixTest/TimeofTxOnChain.java
+++ wutongchain/src/test/java/com/tjfintech/common/functionTest/mixTest/TimeofTxOnChain.java
@@ -33,21 +33,27 @@ public class TimeofTxOnChain {
     FileOperation fileOperation = new FileOperation();
 
 
+    String Data = "";
+    String response = "";
+    String txHash = "";
+    FileOperation fileOper = new FileOperation();
+    WVMContractTest wvmCt = new WVMContractTest();
+    String ctHash = "123";
+    String wvmFile = "wvm";
+    String orgName = "TestExample";//样例合约中的内部合约名
+    String ctName = "";
+    List ctHashList = new ArrayList();
+
+
     public void searchTxOnChain(String type)  throws Exception{
-        String Data = "";
-        String response = "";
-        String txHash = "";
-        FileOperation fileOper = new FileOperation();
         switch (type) {
             case "1": //存证
                 Data = "\"test\":\"json" + UtilsClass.Random(4) + "\"";
                 txHash = JSONObject.fromObject(store.CreateStore(Data)).getString("data");
                 break;
-            case "2"://合约
-                WVMContractTest wvmCt = new WVMContractTest();
-                String wvmFile = "wvm";
-                String orgName = "TestExample";//样例合约中的内部合约名
-                String ctName = "ontime_" + sdf.format(dt) + RandomUtils.nextInt(100000);
+
+            case "2"://合约安装
+                ctName = "ontime_" + sdf.format(dt) + RandomUtils.nextInt(100000);
                 // 替换原wvm合约文件中的合约名称，防止合约重复导致的问题
                 // 替换后会重新生成新的文件名多出"_temp"的文件作为后面合约安装使用的文件
                 fileOper.replace(tempWVMDir + wvmFile + ".txt", orgName, ctName);
@@ -55,40 +61,23 @@ public class TimeofTxOnChain {
                 //安装合约后会得到合约hash：由Prikey和ctName进行运算得到
                 String response1 = wvmCt.wvmInstallTest(wvmFile + "_temp.txt","");
                 txHash = JSONObject.fromObject(response1).getJSONObject("data").getString("txId");
-                String ctHash = JSONObject.fromObject(response1).getJSONObject("data").getString("name");
+                ctHashList.add(ctHash);
+                break;
 
+            case "3"://合约调用
+                String response2 = wvmCt.invokeNew(ctHash,"initAccount","AA","100");//初始化账户A 账户余额50
+                txHash = JSONObject.fromObject(response2).getJSONObject("data").getString("txId");
+                break;
+            case "4"://合约销毁
+                String response3 = wvmCt.wvmDestroyTest(ctHash);//初始化账户A 账户余额50
+                txHash = JSONObject.fromObject(response3).getJSONObject("data").getString("txId");
+                break;
         }
         long nowTime = new Date().getTime();
         GetTxDetail(nowTime,txHash);
     }
 
 
-    //@Test
-    public void searchUTXOInlocal()  throws Exception{
-        String tokenType1 = "zz1X-" + UtilsClass.Random(10);
-        String tokenType2 = "q1qX-" + UtilsClass.Random(10);
-//        log.info("*****************************"+tokenType1);
-//        log.info("*****************************"+tokenType2);
-        String response = multiSign.issueToken(IMPPUTIONADD, tokenType1, "1000", "issue");
-        assertThat(response, containsString("200"));
-        String Tx1 = JSONObject.fromObject(response).getJSONObject("data").getString("tx");
-
-        String response3 = multiSign.issueToken(IMPPUTIONADD, tokenType2, "5000", "issue");
-        assertThat(response3, containsString("200"));
-        String Tx2 = JSONObject.fromObject(response3).getJSONObject("data").getString("tx");
-        String response2 = multiSign.Sign(Tx1, PRIKEY5);
-        assertThat(response2, containsString("200"));
-        String response4 = multiSign.Sign(Tx2, PRIKEY5);
-        assertThat(response4, containsString("200"));
-
-        long nowTime = new Date().getTime();
-        JSONObject jsonObject1=JSONObject.fromObject(response4);
-        String storeHash2 = jsonObject1.getJSONObject("data").get("txId").toString();
-
-        GetTxDetail(nowTime,storeHash2);
-        nowTime = new Date().getTime();
-        GetLocal(nowTime,storeHash2);
-    }
     public void GetTxDetail(long time ,String hash) throws Exception{
 
         String response2= store.GetTxDetail(hash);
@@ -118,12 +107,6 @@ public class TimeofTxOnChain {
         }
     }
 
-//    @Test
-//    public  void UTXOOnChainTimeTest()throws Exception{
-//        for(int i=0;i<500;i++){
-//            searchUTXOInlocal();
-//        }
-//    }
 
     @Test
     public  void OnChainTimeTest()throws Exception{
@@ -136,9 +119,33 @@ public class TimeofTxOnChain {
         }
 
         for(int i=0;i<1000;i++){
-            log.info("++++++++++++++++++++++++++++++++++++++++++++++++wvm " + i);
+            log.info("++++++++++++++++++++++++++++++++++++++++++++++++wvm install " + i);
             searchTxOnChain("2");
         }
+
+        for(int i=0;i<1000;i++){
+            log.info("++++++++++++++++++++++++++++++++++++++++++++++++wvm destory" + i);
+            ctHash = ctHashList.get(i).toString();
+            searchTxOnChain("4");
+        }
+
+
+        //合约调用
+        String ctName = "ontime_" + sdf.format(dt) + RandomUtils.nextInt(100000);
+        // 替换原wvm合约文件中的合约名称，防止合约重复导致的问题
+        // 替换后会重新生成新的文件名多出"_temp"的文件作为后面合约安装使用的文件
+        fileOper.replace(tempWVMDir + wvmFile + ".txt", orgName, ctName);
+
+        //安装合约后会得到合约hash：由Prikey和ctName进行运算得到
+        String response1 = wvmCt.wvmInstallTest(wvmFile + "_temp.txt","");
+        ctHash = JSONObject.fromObject(response1).getJSONObject("data").getString("name");
+        sleepAndSaveInfo(2000);
+
+        for(int i=0;i<1000;i++){
+            log.info("++++++++++++++++++++++++++++++++++++++++++++++++wvm " + i);
+            searchTxOnChain("3");
+        }
+
     }
 
 //    @Test
