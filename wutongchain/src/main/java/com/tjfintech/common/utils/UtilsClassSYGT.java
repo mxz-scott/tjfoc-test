e33b9eee6b207110b59c6a88a2d3c45efebc5c9d
dduzou <zoulili@wutongchain.com>
2021-04-16 11:28:42 +0800

    上银国泰 更新盟主机构代码

--- wutongchain/src/main/java/com/tjfintech/common/utils/UtilsClassSYGT.java
+++ wutongchain/src/main/java/com/tjfintech/common/utils/UtilsClassSYGT.java
@@ -10,8 +10,8 @@ import static com.tjfintech.common.utils.UtilsClass.testDataPath;
 @Slf4j
 public class UtilsClassSYGT {
 
-    public static String code1 = "shanghai-bank"; //盟主1
-    public static String code2 = "gtja"; //盟主2
+    public static String code1 = "BOS"; //盟主1
+    public static String code2 = "GTJA"; //盟主2
     public static String code3 = "m3" + UtilsClass.Random(6); //成员1
 
     public static String name1 = "上海银行"; //盟主1
