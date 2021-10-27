283395947d16300ddfb3ecc42c3fee012689fe36
dduzou <zoulili@wutongchain.com>
2021-10-27 13:18:53 +0800

    教育项目 增加环境信息

--- wutongchain/src/main/java/com/tjfintech/common/utils/UtilsClass.java
+++ wutongchain/src/main/java/com/tjfintech/common/utils/UtilsClass.java
@@ -51,6 +51,7 @@ public class UtilsClass {
             case "5":  SetTestEnv_zllauto envSet05 = new SetTestEnv_zllauto(); envSet05.setEnvParam(); break;
             case "6":  SetTestEnv_zllManual envSet06 = new SetTestEnv_zllManual(); envSet06.setEnvParam(); break;
             case "7":  SetTestEnv_xlh envSet07 = new SetTestEnv_xlh(); envSet07.setEnvParam(); break;
+            case "8":  SetTestEnv_edu envSet08 = new SetTestEnv_edu(); envSet08.setEnvParam(); break;
 
             default: log.info("使用初始化默认参数");
         }
