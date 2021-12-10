3eb45220db8876dc96593a269dc2eded9140c88b
lilu646651065 <lilu@wutongchain.com>
2021-12-10 13:48:44 +0800

    更新lilu环境配置

--- wutongchain/src/main/java/com/tjfintech/common/PersonalTestEnvironment/SetTestEnv_lilu.java
+++ wutongchain/src/main/java/com/tjfintech/common/PersonalTestEnvironment/SetTestEnv_lilu.java
@@ -8,90 +8,90 @@ import static com.tjfintech.common.utils.UtilsClass.*;
 @Slf4j
 public class SetTestEnv_lilu {
 
-   @Test
-    public void setEnvParam(){
+    @Test
+    public void setEnvParam() {
 
-     //***************************************************************zhengxin**********************************************************************************
-//     public static String ADD = "http://10.1.5.225:8088";
-//     public static String SDKADD = ADD;
-//     public static String rSDKADD = ADD;
-//     public static String TOKENADD = ADD;
+        //***************************************************************zhengxin**********************************************************************************
+
+        SDKADD = "http://10.1.5.225:8088";
+        rSDKADD = "http://10.1.5.225:8088";
+        TOKENADD = "http://10.1.5.225:8088";
+
+        //设置测试环境使用的节点端口及部署目录信息
+        PEER1IP = "10.1.5.225";
+        PEER2IP = "10.1.5.226";
+        PEER3IP = "10.1.3.161";
+        PEER4IP = "10.1.5.227";
+        PEER1RPCPort = "8200";
+        PEER2RPCPort = "8200";
+        PEER3RPCPort = "8200";
+        PEER4RPCPort = "8200";
+        PEER1TCPPort = "8000";
+        PEER2TCPPort = "8000";
+        PEER3TCPPort = "8000";
+        PEER4TCPPort = "8000";
+        //节点、SDK、Toolkit对等目录放置于PTPATH目录下
+        PTPATH = "/opt/";
+        SDKPATH = PTPATH + "zxsdk/";
+        PeerPATH = PTPATH + "zxchain/";
+        ToolPATH = PTPATH + "wttool/";
+        TokenApiPATH = PTPATH + "wtfinservice/";
+        PeerTPName = "wtchain";
+        SDKTPName = "wtsdk";
+        ToolTPName = "wttool";
+        TokenTPName = "wtfinservice";
+        tmuxSessionTokenApi = "tmux send -t zx-sdk ";
+        tmuxSessionPeer = "tmux send -t zx-chain ";
+        tmuxSessionSDK = "tmux send -t zx-sdk ";
+        sReleaseLocalDir = "E:\\gopath\\src\\github.com\\tjfoc\\wtsys-release\\release\\梧桐链已发布版本\\2.3\\";
+        sLatestLocalDir = "E:\\test\\2.4.2\\";
+
+
+        //    public static String resourcePath = System.getProperty("user.dir") + "/src/main/resources/";
+        testResultPath = "testresult/";
+        testDataPath = "testdata/";
+        srcShellScriptDir = testDataPath + "/configFiles/shell/";
+        destShellScriptDir = "/opt/tjshell/";
+        tempWVMDir = testDataPath + "WVM/";
+
+
+        //***************************************************************应用链**********************************************************************************
+//     SDKADD = "http://10.1.5.225:7088";
+//     rSDKADD = "http://10.1.5.225:7088";
+//     TOKENADD = "http://10.1.5.225:7088";
 //
 //     //设置测试环境使用的节点端口及部署目录信息
-//     public static String PEER1IP = "10.1.5.225";
-//     public static String PEER2IP = "10.1.5.226";
-//     public static String PEER3IP = "10.1.3.161";
-//     public static String PEER4IP = "10.1.5.227";
-//     public static String PEER1RPCPort = "8200";
-//     public static String PEER2RPCPort = "8200";
-//     public static String PEER3RPCPort = "8200";
-//     public static String PEER4RPCPort = "8200";
-//     public static String PEER1TCPPort = "8000";
-//     public static String PEER2TCPPort = "8000";
-//     public static String PEER3TCPPort = "8000";
-//     public static String PEER4TCPPort = "8000";
+//     PEER1IP = "10.1.5.225";
+//     PEER2IP = "10.1.5.226";
+//     PEER3IP = "10.1.3.161";
+//     PEER4IP = "10.1.5.227";
+//     PEER1RPCPort = "7200";
+//     PEER2RPCPort = "7200";
+//     PEER3RPCPort = "7200";
+//     PEER4RPCPort = "7200";
+//     PEER1TCPPort = "7000";
+//     PEER2TCPPort = "7000";
+//     PEER3TCPPort = "7000";
+//     PEER4TCPPort = "7000";
 //     //节点、SDK、Toolkit对等目录放置于PTPATH目录下
-//     public static String PTPATH = "/opt/";
-//     public static String SDKPATH = PTPATH + "zxsdk/";
-//     public static String PeerPATH = PTPATH + "zxchain/";
-//     public static String ToolPATH = PTPATH + "wttool/";
-//     public static String TokenApiPATH = PTPATH + "wtfinservice/";
-//     public static String PeerTPName = "wtchain";
-//     public static String SDKTPName = "wtsdk";
-//     public static String ToolTPName = "wttool";
-//     public static String TokenTPName = "wtfinservice";
-//     public static String tmuxSessionTokenApi = "tmux send -t zx-sdk ";
-//     public static String tmuxSessionPeer = "tmux send -t zx-chain ";
-//     public static String tmuxSessionSDK = "tmux send -t zx-sdk ";
-//     public static String sReleaseLocalDir = "E:\\gopath\\src\\github.com\\tjfoc\\wtsys-release\\release\\梧桐链已发布版本\\2.3\\";
-//     public static String sLatestLocalDir = "E:\\test\\2.4.2\\";
-//
-//
-//     //    public static String resourcePath = System.getProperty("user.dir") + "/src/main/resources/";
-//     public static String testResultPath = "testresult/";
-//     public static String testDataPath = "testdata/";
-//     public static String srcShellScriptDir = testDataPath + "/configFiles/shell/";
-//     public static String destShellScriptDir = "/opt/tjshell/";
-//     public static String tempWVMDir = testDataPath + "WVM/";
-//
-//
-     //***************************************************************应用链**********************************************************************************
-     SDKADD = "http://10.1.5.225:7088";
-     rSDKADD = "http://10.1.5.225:7088";
-     TOKENADD = "http://10.1.5.225:7088";
-
-     //设置测试环境使用的节点端口及部署目录信息
-     PEER1IP = "10.1.5.225";
-     PEER2IP = "10.1.5.226";
-     PEER3IP = "10.1.3.161";
-     PEER4IP = "10.1.5.227";
-     PEER1RPCPort = "7200";
-     PEER2RPCPort = "7200";
-     PEER3RPCPort = "7200";
-     PEER4RPCPort = "7200";
-     PEER1TCPPort = "7000";
-     PEER2TCPPort = "7000";
-     PEER3TCPPort = "7000";
-     PEER4TCPPort = "7000";
-     //节点、SDK、Toolkit对等目录放置于PTPATH目录下
-     PTPATH = "/opt/";
-     SDKPATH = PTPATH + "wtsdk/";
-     PeerPATH = PTPATH + "wtchain/";
-     ToolPATH = PTPATH + "wttool/";
-     TokenApiPATH = PTPATH + "wtfinservice/";
-     PeerTPName = "wtchain";
-     SDKTPName = "wtsdk";
-     ToolTPName = "wttool";
-     TokenTPName = "wtfinservice";
-     tmuxSessionTokenApi = "tmux send -t wt-sdk ";
-     tmuxSessionPeer = "tmux send -t wt-chain ";
-     tmuxSessionSDK = "tmux send -t wt-sdk ";
-     sReleaseLocalDir = "E:\\\\test\\\\2.3\\\\";
-     sLatestLocalDir = "E:\\\\test\\\\2.4.2\\\\";
-     destShellScriptDir = "/opt/tjshell/";
+//     PTPATH = "/opt/";
+//     SDKPATH = PTPATH + "wtsdk/";
+//     PeerPATH = PTPATH + "wtchain/";
+//     ToolPATH = PTPATH + "wttool/";
+//     TokenApiPATH = PTPATH + "wtfinservice/";
+//     PeerTPName = "wtchain";
+//     SDKTPName = "wtsdk";
+//     ToolTPName = "wttool";
+//     TokenTPName = "wtfinservice";
+//     tmuxSessionTokenApi = "tmux send -t wt-sdk ";
+//     tmuxSessionPeer = "tmux send -t wt-chain ";
+//     tmuxSessionSDK = "tmux send -t wt-sdk ";
+//     sReleaseLocalDir = "E:\\\\test\\\\2.3\\\\";
+//     sLatestLocalDir = "E:\\\\test\\\\2.4.2\\\\";
+//     destShellScriptDir = "/opt/tjshell/";
 
 
-     //**********************************************************************pufa****************************************************************************
+        //**********************************************************************pufa****************************************************************************
 //       SDKADD = "http://10.1.5.225:7082";
 //       rSDKADD = "http://10.1.5.225:7082";
 //       TOKENADD = "http://10.1.5.225:7082";
@@ -126,6 +126,6 @@ public class SetTestEnv_lilu {
 //       sLatestLocalDir = "E:\\test\\2.4.2\\";
 //      destShellScriptDir = "/opt/tjshell/";
 
-   }
+    }
 
 }
