f47f306dc1c1a5bf2918bf108ebcb79e3add1f6d
lilu646651065 <lilu@wutongchain.com>
2022-01-13 16:27:57 +0800

    更新跑批wvm合约调用脚本

--- wutongchain/src/test/java/com/tjfintech/common/stableTest/Temp_Test.java
+++ wutongchain/src/test/java/com/tjfintech/common/stableTest/Temp_Test.java
@@ -2,23 +2,18 @@ package com.tjfintech.common.stableTest;
 
 import com.tjfintech.common.BeforeCondition;
 import com.tjfintech.common.CommonFunc;
-import com.tjfintech.common.Interface.MultiSign;
-import com.tjfintech.common.Interface.SoloSign;
-import com.tjfintech.common.Interface.Store;
-import com.tjfintech.common.Interface.Token;
+import com.tjfintech.common.Interface.*;
 import com.tjfintech.common.TestBuilder;
 import com.tjfintech.common.functionTest.mixTest.VerifyTests;
 import com.tjfintech.common.functionTest.smartTokenTest.SmartTokenCommon;
 import com.tjfintech.common.utils.UtilsClass;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.json.JSONObject;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.tjfintech.common.utils.UtilsClass.*;
 import static org.hamcrest.Matchers.containsString;
@@ -30,6 +25,7 @@ import static org.junit.Assert.assertThat;
 public class Temp_Test {
     TestBuilder testBuilder = TestBuilder.getInstance();
     Store store = testBuilder.getStore();
+    Contract contract = testBuilder.getContract();
     UtilsClass utilsClass = new UtilsClass();
     CommonFunc commonFunc = new CommonFunc();
 
@@ -70,9 +66,9 @@ public class Temp_Test {
                 String response2 = store.GetStorePost(StoreHashPwd, PRIKEY1);
                 code = JSONObject.fromObject(response2).getInt("state");
                 i++;
-                if ( code == 200 ) break;
+                if (code == 200) break;
                 Thread.sleep(sleep);
-            }while (code != 200);
+            } while (code != 200);
 
             times[j] = i * sleep;
 
@@ -80,11 +76,45 @@ public class Temp_Test {
 
         int i = 0;
         for (int t : times) {
-            if (t > sleep ){
+            if (t > sleep) {
                 log.info("查询到结果耗时 " + (500 + t) + " 毫秒");
                 i++;
             }
         }
         log.info("一共 " + i + "  次超时");
+    }
+
+    /**
+     * 循环执行WVM合约调用，写入数据
+     */
+    @Test
+    public void WVMContractInvokeBatch() throws Exception {
+
+        String file = "Ly/ouqvku73orqTor4HlkIjnuqYKY29udHJhY3QgSWRlbnRpdHkgewoKICAgIC8v6Lqr5Lu957uT5p6E5L2TCiAgICBzdHJ1Y3QgVXNlckluZm8gewoKICAgICAgICBzdHJpbmcgTmFtZSAgICAgICAgIC8v5aeT5ZCNCiAgICAgICAgc3RyaW5nIElEICAgICAgICAgICAvL+i6q+S7veivgeWPtwogICAgICAgIHN0cmluZyBBZ2UgICAgICAgICAgLy/lubTpvoQKICAgICAgICBzdHJpbmcgRGF0YSAgICAgICAgIC8v5pWw5o2uCgogICAgfQogIC8v5re75Yqg55So5oi35L+h5oGvCiAgICBwdWJsaWMgc3RyaW5nIEFkZFVzZXJJbmZvIChzdHJpbmcgdXNlckluZm9Kc29uKSB7CgogICAgICAgIFVzZXJJbmZvIHVpbmZvID0ganNvbl90b19vYmo8VXNlckluZm8+KHVzZXJJbmZvSnNvbikKICAgICAgICBzdHJpbmcga2V5ID0gInVzZXJfIit1aW5mby5JRCAKICAgICAgICBkYl9zZXQoa2V5LHVzZXJJbmZvSnNvbikKICAgICAgICBwcmludChrZXkrdXNlckluZm9Kc29uKQogICAgICAgIHJldHVybigic3VjY2VzcyIpCgogICAgfQogICAgLy/ojrflj5bnlKjmiLfkv6Hmga8KICAgIHB1YmxpYyBzdHJpbmcgR2V0VXNlckluZm8oc3RyaW5nIGtleSkgewogICAgICAgIHN0cmluZyBpbmZvID0gZGJfZ2V0PHN0cmluZz4oa2V5KQogICAgICAgIHByaW50KGluZm8pCiAgICAgICAgcmV0dXJuIGluZm8gCiAgICB9CiAgICAvL+WQiOe6puWIneWni+WMlgogICAgcHVibGljIHN0cmluZyBpbml0KCkgewogICAgICAgIHJldHVybiAiaW5pdCBzdWNjZXNzIgogICAgfQoKCn0=";
+        String response = contract.InstallWVM(file, "wvm", "");
+        assertEquals("200", JSONObject.fromObject(response).getString("state"));
+        String contractName = JSONObject.fromObject(response).getJSONObject("data").getString("name");
+        commonFunc.sdkCheckTxOrSleep(commonFunc.getTxHash(globalResponse, utilsClass.sdkGetTxHashType20),
+                utilsClass.sdkGetTxDetailTypeV2, SLEEPTIME);
+
+        List<Object> args = new LinkedList<>();
+        int i = 10;
+        for (int j = 1; j < i; j++) {
+            String ID = System.currentTimeMillis() + UtilsClass.Random(6);
+            String arg = " {\"Name\":\"123\",\"ID\":\"" + ID + "\",\"Age\":\"20\",\"Data\":\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc\"}";
+
+            args.clear();
+            args.add(arg);
+            response = contract.Invoke(contractName, "", "wvm", "AddUserInfo", args);
+            assertEquals("200", JSONObject.fromObject(response).getString("state"));
+            commonFunc.sdkCheckTxOrSleep(commonFunc.getTxHash(globalResponse, utilsClass.sdkGetTxHashType20),
+                    utilsClass.sdkGetTxDetailTypeV2, SLEEPTIME);
+            args.clear();
+            args.add("user_" + ID);
+            response = contract.QueryWVM(contractName, "", "wvm", "GetUserInfo", "", args);
+            assertEquals("200", JSONObject.fromObject(response).getString("state"));
+            assertEquals(true, response.contains(ID));
+            log.info("---------------------invoke " + j + " success-------------------------");
+        }
     }
 }
